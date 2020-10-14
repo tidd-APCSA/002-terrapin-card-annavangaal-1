fa21edd90005172627c9ae13cba7b2755b2a39a3
annavangaal <annavangaal@yahoo.com>
2020-10-14 13:54:36 +0000

    Updated TerrapinCard class.

--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,4 +1,21 @@
 public class TerrapinCard {
 
+  private double balance;
 
+  public TerrapinCard(double balance) {
+    this.balance = balance;
+  }
+
+  public String toString() {
+    return "The card has " + balance + " dollars";
+  }
+
+  public void payEconomical(){
+    balance = balance - 2.50;
+  }
+
+  public void payGourmet(){
+    balance = balance - 4.00;
+  } 
 }
+
